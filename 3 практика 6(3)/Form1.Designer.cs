1d743690853298a5bdeb7968af9c04b838a252d8
student43 <studkab43@jdk.ru>
2019-02-04 09:33:33 +0500

    Добавьте файлы проекта.

---
+++ 3 практика 6(3)/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _3_практика_6_3_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "доллар",
+            "евро",
+            "фунт"});
+            this.listBox1.Location = new System.Drawing.Point(12, 104);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 43);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Расчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 454);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 3 практика 6(3)/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_практика_6_3_
+{
+    public partial class Form1 : Form
+    {
+        class Valuta
+        {
+            public static double ValDollar(double a1)
+            {
+                double p;
+                p = a1 * 66;
+                return a1 * 66;
+            }
+            public static double ValEvro(double a1)
+            {
+                double p;
+                p = a1 * 74;
+                return a1 * 74;
+            }
+            
+            public static double ValFynt(double a1)
+            {
+                double p;
+                p = a1 * 86;
+                return a1 * 86;
+            }
+        }
+        
+            public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a1, s = 0;
+            a1 = Convert.ToDouble(textBox1.Text);
+
+            if (listBox1.Text == "доллар")
+            {
+                s = Valuta.ValDollar(a1);
+                label1.Text = a1 + " руб " + " = " + Convert.ToString(s) + " долларов";
+            }
+            else
+                if (listBox1.Text == "евро")
+            {
+                s = Valuta.ValEvro(a1);
+                label1.Text = a1 + " руб " + " = " + Convert.ToString(s) + " евро";
+            }
+            
+               
+            else
+                        if (listBox1.Text == "фунт")
+            {
+                s = Valuta.ValFynt(a1);
+                label1.Text = a1 + " руб " + " = " + Convert.ToString(s) + " фунтов";
+            }
+            else
+            {
+                s = a1;
+                label1.Text = a1 + " руб " + " = " + Convert.ToString(s) + " грн";
+            }
+        }
+    }
+}
